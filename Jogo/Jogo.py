f3c81f53ed3522658f8d6eaeae219335b7cd390c
Guilherme Gamaliel Lima <111901785+GuilhermeGamaliel@users.noreply.github.com>
2025-05-30 01:58:58 -0300

    Add files via upload

--- Jogo/Jogo.py
+++ Jogo/Jogo.py
@@ -1,35 +1,60 @@
 import pygame
 from pygame import display, mixer, event, draw, font, image, Rect
-from pygame.locals import QUIT, KEYUP, K_SPACE
+from pygame.locals import QUIT
 from pygame.time import Clock
 from pygame.transform import scale
 from random import randint
+#import playground
+
+# O código que eu vou colocar abaixo era para funcionar como módulo do arquivo playground
+# Mas como é 1:30 da manhã e eu não sei porque não tá funcionando eu vou colocar aqui mesmo
+# é uma boa ideia tirar depois
+
+#parte que preenche as listas
+def preenchedor(nums1, nums2, teto, qnt_perguntas):
+    for i in range(0, qnt_perguntas):
+        nums1.append(randint(0,teto))
+        nums2.append(randint(0,teto))
+
+# A função somador tem a importante tarefa de construir a lista soma_nat
+# A lista soma_nat funciona como um gabarito para as questões de soma
+def somador(nums1, nums2, soma_nat, qnt_perguntas):
+    preenchedor(nums1, nums2, teto, qnt_perguntas)
+    for i in range(0, qnt_perguntas):
+        soma_nat[i] = nums1[i] + nums2[i]
 
 pygame.init()
+
+# Música de fundo :)
 mixer.init()
 pygame.mixer.music.load("chopin.mp3")
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.play(-1) 
+# A última linha tá dentro de um comentário, porque a música tava chata e eu mandei parar de tocar XD
+
+# Estado é a variável que carrega consigo a tela na qual estamos
 estado = 0
+
+fases = 15 
+secoes = 4
+
+# Variaveis que carregam as proporções da tela
+# A ideia é que o jogo seja responsivo
 info = display.Info()
 largura = info.current_w
 altura = info.current_h
-centro_da_tela = largura/2
+
 tela = display.set_mode((altura, largura), pygame.FULLSCREEN)
 display.set_caption("Praticando matemática")
 
-
-
-
-#ret = pygame.Rect(centro_da_tela,centro_da_tela,(100,100))
-#draw.rect(tela, (255,255,255), ret)
-
+# variaveis da pagina das fases
+blocos_horizontal = 5
+blocos_vertical = 3
+blocos_total = blocos_horizontal * blocos_vertical
 
 display.flip()
 
-
-
 while True:
-    for evento in pygame.event.get():
+    for evento in event.get():
         if evento.type == QUIT:
             pygame.quit()
             exit()
@@ -69,18 +94,60 @@ while True:
             texto = fonte_texto.render("Fechar o jogo", True, (0,0,0))
             tela.blit(texto,((largura/8),(altura*3/4)))
 
+            #Imagem do protagonista do jogo
             imagem = image.load("Menino com lápis gigante alegre.jpg")
+            #pygame.transform.scale(imagem,)
             tela.blit(imagem,((largura*3/4),(altura/2)))
+            
         case 1:
+            #Essa é a tela que tem as fases do jogo
             tela.fill((0, 255, 255))
 
+            texto = fonte_titulo.render("Primeira parte do jogo(Adição)",True,(0,0,0))
+            tela.blit(texto, ((largura/32),(altura/32)))
+
+            tamanho_ret, pos_ret_x = largura/8, largura/8
+            pos_ret_y = altura/8
+
+            # O trambolho abaixo tá colocando as fases na tela
+            
+            blocos = []
+            
+            for i in range(1, blocos_vertical+1):
+                for j in range(1, blocos_horizontal+1):
+                    # Desenha a fase/bloco na tela
+                    rect = Rect(j*(pos_ret_x+tamanho_ret/8), i*(pos_ret_y + tamanho_ret/2), tamanho_ret, tamanho_ret)
+                    draw.rect(tela, (0, 200, 200), rect, 0, 25)
+                    fase_id = i*j
+
+                    # Enumera as fases
+                    texto = fonte_texto.render(str(i*j),True,(0,0,0))
+                    tela.blit(texto,(j*(pos_ret_x+tamanho_ret/8), i*(pos_ret_y + tamanho_ret/2)))
+
+            for evento in event.get():
+                mouse_pos = evento.pos
+                rect = Rect((pos_ret_x+tamanho_ret/8),(pos_ret_y + tamanho_ret/2), tamanho_ret, tamanho_ret)
+                if rect.collidepoint(mouse_pos):
+                    estado = 2
+        case 2:
+            tela.fill((0,250,250))
 
             
-            pass
-            
+            nums1,nums2,teto,num_perguntas = [],[],9,15
+            preenchedor(nums1,nums2,teto,num_perguntas)
+            for i in range(0,num_perguntas):
+                texto = fonte_titulo.render(f"{nums1[i]} + {nums2[i]}", True, (0,0,0))
+                user_text = fonte_titulo.render(" ", True, (0,0,0))
+                tela.blit(texto,(largura/2,altura/2))
+                tela.blit(user_text,(largura/2,altura*3/4))
+                for evento in event.get():
+                    if evento.type == pygame.KEYDOWN:
+                        user_text += evento.unicode
+                
+
             
             
 
-
+                
     display.update()
 
--- Jogo/playground.py
+++ Jogo/playground.py
@@ -1,43 +1,50 @@
 from random import randint 
 
+# A ideia desse arquivo maravilhoso aqui é servir como um módulo do arquivo Jogo.py
+# Entretanto se você tirar o final do código que está comentado do comentário ele funciona como um jogo a parte
+# Vale entretanto ressaltar que esse joguinho aqui não estava funcionando da última vez que eu testei só para deixar bem claro
+
 nums1, nums2, soma_nat, subt_nat, mult_nat, div_nat, respostas = {}, {}, {}, {}, {}, {}, {}
 qnt_acerto = 0
 qnt_perguntas = 10
 teto = 9
 
 #parte que preenche as listas
-def preenchedor(nums1, nums2, teto):
-    aux = 0
-    for i in range(qnt_perguntas):
+def preenchedor(nums1, nums2, teto, qnt_perguntas):
+    for i in range(0, qnt_perguntas):
         nums1[i] = randint(0,teto)
         nums2[i] = randint(0,teto)
 
 # A função somador tem a importante tarefa de construir a lista soma_nat
 # A lista soma_nat funciona como um gabarito para as questões de soma
-def somador(nums1, nums2, soma_nat):
-    preenchedor(nums1, nums2, teto)
-    for i in range(qnt_perguntas):
+def somador(nums1, nums2, soma_nat, qnt_perguntas):
+    preenchedor(nums1, nums2, teto, qnt_perguntas)
+    for i in range(0, qnt_perguntas):
         soma_nat[i] = nums1[i] + nums2[i]
 
 # A função subtraidor tem uma função parecida com a função somador
 # Mas nesse caso ela constrói a lista sub_nat
-def subtraidor(nums1, nums2, subt_nat):
-    preenchedor(nums1, nums2, teto)
-    if nums1[i] < nums2[i]:
-        aux = nums1[i]
-        nums1[i] = nums2[i]
-        nums2[i] = aux
+def subtraidor(nums1, nums2, subt_nat, qnt_perguntas):
+    preenchedor(nums1, nums2, teto, qnt_perguntas)
+    for i in range(0, qnt_perguntas):
+        if nums1[i] < nums2[i]:
+            aux = nums1[i]
+            nums1[i] = nums2[i]
+            nums2[i] = aux
     subt_nat[i] = nums1[i] - nums2[i]
 
 def multiplicador(nums1, nums2, mult_nat):
     teto = 10
-    preenchedor(nums1, nums2, teto)
-    for i in range(qnt_perguntas):
+    preenchedor(nums1, nums2, teto, qnt_perguntas)
+    for i in range(0, qnt_perguntas):
         mult_nat[i] = nums1[i] * nums2[i]
 
 
-somador(nums1, nums2, soma_nat)
+
+"""
+somador(nums1, nums2, soma_nat, qnt_perguntas)
 #parte que faz as perguntas
+
 print("Fase 1 (Adição): ")
 for i in range(qnt_perguntas):
     print("Quanto é ", nums1[i]," + ", nums2[i],"?")
@@ -49,6 +56,7 @@ for i in range(qnt_perguntas):
 
 print("Voce acertou:", qnt_acerto, "perguntas") 
 qnt_acerto = 0
+
 
 subtraidor(nums1,nums2,subt_nat)
 print("Fase 2 (subtração): ")
@@ -74,3 +82,4 @@ for i in range(qnt_perguntas):
 print("Voce acertou:", qnt_acerto, "perguntas") 
 qnt_acerto = 0
 
+"""
